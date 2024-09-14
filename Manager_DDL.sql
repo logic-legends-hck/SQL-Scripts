a20efbaac513064243c1def734740fc97d571146
logic-legends-hck <rizzpy107@gmail.com>
2024-09-14 20:17:10 +0530

    Add file via Python

--- Manager_DDL.sql
+++ Manager_DDL.sql
@@ -3,11 +3,11 @@
 
 CREATE TABLE private.Manager (
     Mngr_id Integer NOT NULL DEFAULT -99999.0,
-    Name Integer NOT NULL,
+    Name Varchar(50) NOT NULL,
     Age Integer,
-    Birth_Dt Integer,
+    Birth_Dt Varchar(50),
     Gender Integer,
-    Email Integer,
+    Email Varchar(50),
     Phone Integer,
     PRIMARY KEY (Mngr_id)
 );
