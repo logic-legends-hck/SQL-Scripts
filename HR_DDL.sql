ace6bb761230070cf988d8b21706c7fda75eba84
logic-legends-hck <rizzpy107@gmail.com>
2024-09-18 11:10:32 +0530

    Add file via Python

--- HR_DDL.sql
+++ HR_DDL.sql
@@ -1,7 +1,7 @@
 --liquibase format
---CHANGESET HR_DDL: 05 
+--CHANGESET HR_DDL: 06 
 
-CREATE TABLE private.HR (
+CREATE TABLE insurance_agency.HR (
     Mngr_id Integer NOT NULL DEFAULT -99999.0,
     Name Integer NOT NULL,
     Age Integer,
@@ -12,8 +12,8 @@ CREATE TABLE private.HR (
     PRIMARY KEY (Mngr_id)
 );
 
-GRANT SELECT, UPDATE, INSERT ON table private.HR TO GROUP "$redshift_group_rw";
-GRANT SELECT ON table private.HR TO GROUP "$redshift_group_ro";
-GRANT SELECT, UPDATE, INSERT ON table private.HR TO GROUP "$redshift_group_off";
-GRANT SELECT, UPDATE, INSERT ON table private.HR TO GROUP "$redshift_group_adm";
-GRANT ALL ON table private.HR TO edmetl;
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.HR TO GROUP "$redshift_group_rw";
+GRANT SELECT ON table insurance_agency.HR TO GROUP "$redshift_group_ro";
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.HR TO GROUP "$redshift_group_off";
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.HR TO GROUP "$redshift_group_adm";
+GRANT ALL ON table insurance_agency.HR TO edmetl;
