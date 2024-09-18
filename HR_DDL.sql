275785b9901cee6c4a0b8b78035b2568568f22ec
logic-legends-hck <rizzpy107@gmail.com>
2024-09-18 11:03:35 +0530

    Add file via Python

---
+++ HR_DDL.sql
@@ -0,0 +1,19 @@
+--liquibase format
+--CHANGESET HR_DDL: 01 
+
+CREATE TABLE private.HR (
+    Mngr_id Integer NOT NULL DEFAULT -99999.0,
+    Name Integer NOT NULL,
+    Age Integer,
+    Birth_Dt Integer,
+    Gender Integer,
+    Email Integer,
+    Phone Integer,
+    PRIMARY KEY (Mngr_id)
+);
+
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_rw;
+GRANT SELECT ON table {schema_name}.{table_name} TO GROUP $redshift_group_ro;
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_off;
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_adm;
+GRANT ALL ON table {schema_name}.{table_name} TO edmetl;
