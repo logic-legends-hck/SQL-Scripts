7a2cb98366a2c24efe32c2efb6f7333b8b3022e3
logic-legends-hck <rizzpy107@gmail.com>
2024-09-18 11:10:37 +0530

    Add file via Python

--- Student_DDL.sql
+++ Student_DDL.sql
@@ -1,7 +1,7 @@
 --liquibase format
---CHANGESET Student_DDL: 05 
+--CHANGESET Student_DDL: 06 
 
-CREATE TABLE private.Student (
+CREATE TABLE insurance_agency.Student (
     Mngr_id Integer NOT NULL DEFAULT -99999.0,
     Name Integer NOT NULL,
     Age Integer,
@@ -12,8 +12,8 @@ CREATE TABLE private.Student (
     PRIMARY KEY (Mngr_id)
 );
 
-GRANT SELECT, UPDATE, INSERT ON table private.Student TO GROUP "$redshift_group_rw";
-GRANT SELECT ON table private.Student TO GROUP "$redshift_group_ro";
-GRANT SELECT, UPDATE, INSERT ON table private.Student TO GROUP "$redshift_group_off";
-GRANT SELECT, UPDATE, INSERT ON table private.Student TO GROUP "$redshift_group_adm";
-GRANT ALL ON table private.Student TO edmetl;
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.Student TO GROUP "$redshift_group_rw";
+GRANT SELECT ON table insurance_agency.Student TO GROUP "$redshift_group_ro";
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.Student TO GROUP "$redshift_group_off";
+GRANT SELECT, UPDATE, INSERT ON table insurance_agency.Student TO GROUP "$redshift_group_adm";
+GRANT ALL ON table insurance_agency.Student TO edmetl;
