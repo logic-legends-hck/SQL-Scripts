f6490252db02ac79e2fb3b8ccfffd4842c20a974
logic-legends-hck <rizzpy107@gmail.com>
2024-09-18 11:03:39 +0530

    Add file via Python

---
+++ Student_DDL.sql
@@ -0,0 +1,19 @@
+--liquibase format
+--CHANGESET Student_DDL: 01 
+
+CREATE TABLE private.Student (
+    Mngr_id Integer NOT NULL DEFAULT -99999.0,
+    Name Integer NOT NULL,
+    Age Integer,
+    Birth_Dt Integer,
+    Gender Integer,
+    Email Integer,
+    Phone Integer,
+    PRIMARY KEY (Mngr_id)
+);
+
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_rw;
+GRANT SELECT ON table {schema_name}.{table_name} TO GROUP $redshift_group_ro;
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_off;
+GRANT SELECT, UPDATE, INSERT ON table {schema_name}.{table_name} TO GROUP $redshift_group_adm;
+GRANT ALL ON table {schema_name}.{table_name} TO edmetl;
